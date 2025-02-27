36b4857337e66885eb57288d34e04f0c8bac0545
Deepak Vaid <dvaid79@gmail.com>
2025-02-27 17:06:35 +0530

    Problems were with 3d viewport. Disabling that fixed the issue. Resizable, draggable, panel app works

--- app/components/Viewport.tsx
+++ app/components/Viewport.tsx
@@ -1,21 +1,11 @@
 "use client"
 
-import dynamic from "next/dynamic"
-
-const Scene = dynamic(() => import("./Scene"), {
-  ssr: false,
-  loading: () => (
-    <div className="flex-grow flex items-center justify-center bg-gray-50">
-      <div className="text-gray-500">Loading 3D viewport...</div>
-    </div>
-  ),
-})
-
 export default function Viewport() {
   return (
-    <div className="flex-grow relative">
-      <Scene />
+    <div className="flex-grow relative bg-gray-50">
+      <div className="flex items-center justify-center h-full text-gray-500">
+        3D Viewport Disabled
+      </div>
     </div>
   )
 }
-
